c0020f7f45d089960f1ca0602c8e71e152b3a1ce
Palli-Moon <pallimoon@gmail.com>
2014-08-28 16:28:18 +0000

    Arrow keys now work

--- Player.cpp
+++ Player.cpp
@@ -7,19 +7,24 @@ Player::Player(Board &b)
 
 void Player::get_command()
 {
+    keypad(stdscr, true);
     wchar_t ch;
     ch = getch();
     switch (ch)
     {
+        case KEY_LEFT:
         case 'a':
             board->move_cursor(LEFT);
             break;
+        case KEY_RIGHT:
         case 'd':
             board->move_cursor(RIGHT);
             break;
+        case KEY_UP:
         case 'w':
             board->move_cursor(UP);
             break;
+        case KEY_DOWN:
         case 's':
             board->move_cursor(DOWN);
             break;
